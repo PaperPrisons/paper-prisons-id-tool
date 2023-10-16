e9f41fdca311fcd12038454b32f0a89f118f2593
blessdyb <blessdyb@gmail.com>
2023-10-15 17:05:41 -0700

    Add docs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: "./",
+  assetPrefix: "https://blessdyb.github.io/paper-prisons-id-tool/",
 };
 
 module.exports = nextConfig;
