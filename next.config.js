384bceeebdf8173c7b8b83e9b524b1aee0df6b85
Sneha Narayanan <snehanarayanan@MacBookAir.attlocal.net>
2026-04-23 12:35:26 -0700

    Remove GitHub Pages asset prefix for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.DEBUG
-    ? null
-    : "https://paperprisons.github.io/paper-prisons-id-tool/",
 };
 
 module.exports = nextConfig;
