b93b8200a13c1d66b2026528e7ebb1b438fd2a74
Navid Shaghaghi <37026687+nshaghaghi@users.noreply.github.com>
2026-06-18 17:04:23 -0700

    Revert "Niharika dev UI work new"

---
+++ docs/_next/static/uplMr1diU3R_5vurTr2fg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/136-74df6688989783e6.js","static/chunks/pages/index-9fd580b1919bfa24.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
